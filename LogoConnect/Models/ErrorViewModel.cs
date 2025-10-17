24e583082234d6e67da29a12297bb8bc7f121445
Zafer <Zafer@DESKTOP-7NVPP3H>
2025-10-17 14:34:18 +0300

    Add project files.

---
+++ LogoConnect/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LogoConnect.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
